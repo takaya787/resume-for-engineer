75f3fac10e55c3a7589e1ef038c2f0413e093df5
takaya787 <takaya318318@gmail.com>
2025-07-09 08:49:55 +0900

    md-to-pdfの設定を更新

--- pdf-configs/config.js
+++ pdf-configs/config.js
@@ -6,11 +6,24 @@ module.exports = {
     smartypants: true,
   },
   pdf_options: {
-    "format": "A4",
-    "margin": "30mm 20mm",
-    "printBackground": true,
-    "headerTemplate": "<style>\n  section {\n    margin: 0 auto;\n    font-size: 9px;\n  }\n</style>",
-    "footerTemplate": "<section>\n  <div>\n    <span class=\"pageNumber\"></span>\n    / <span class=\"totalPages\"></span>\n  </div>\n</section>"
+    format: "A4",
+    margin: "30mm 20mm",
+    printBackground: true,
+    headerTemplate: "<style>\n  section {\n    margin: 0 auto;\n    font-size: 9px;\n  }\n</style>",
+    footerTemplate:
+      '<section>\n  <div>\n    <span class="pageNumber"></span>\n    / <span class="totalPages"></span>\n  </div>\n</section>',
   },
   stylesheet_encoding: "utf-8",
+  launch_options: {
+    // GitHub Actions環境でのサンドボックス問題を解決
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+    ],
+  },
 };
